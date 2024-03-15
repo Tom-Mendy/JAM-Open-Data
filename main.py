e4903981e31bdf3142b758a21c5c5835103cfec7
JsuisSayker <killian.trouve@epitech.eu>
2024-03-15 18:18:36 +0100

    [UPD] main
    add some endpoint that makes an api call to another api for our data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from typing import Union
-
+from fastapi.responses import JSONResponse
+import requests
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -10,6 +11,26 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+list_all = []
+
+
+@app.get("/call_dataset")
+def call_dataset():
+    if list_all != []:
+        list_all.clear()
+    url = "https://geo.api.gouv.fr/communes?fields=codePostaux"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        list_all.append(response_data)
+    else:
+        return JSONResponse(status_code=404, content={
+            "Error": "response not found in the dataset"})
+
+
+@app.get("/random_commune")
+def random_commune():
+    random.seed()
+    random_choice = random.choice(list_all[0])
+    return random_choice
